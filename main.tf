c0cd069972e3d7c2d55ce9c83b3666aa27c28a30
Md Anam Raihan <md.anam.raihan17@gmail.com>
2025-09-01 08:20:31 +0000

    resolve pc

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@
 ##############################################################################
 
 locals {
-  iam_service_id = var.iam_service_provision ? ibm_iam_service_id.service_id[0].id : data.ibm_iam_service_id.service_id_data[0].service_ids[0].id
-  iam_id         = var.iam_service_provision ? ibm_iam_service_id.service_id[0].iam_id : data.ibm_iam_service_id.service_id_data[0].service_ids[0].iam_id
+  iam_id = var.iam_service_provision ? ibm_iam_service_id.service_id[0].iam_id : data.ibm_iam_service_id.service_id_data[0].service_ids[0].iam_id
 }
 
 resource "ibm_iam_service_id" "service_id" {
